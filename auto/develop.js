f61f7a2ac38145f5c8ce6efb90f3be0b297443e4
Hung Tran <hungtran@siliconprime.us>
2016-09-27 14:55:43 +0700

    Init

---
+++ auto/develop.js
@@ -0,0 +1,12 @@
+window._appConfig2 = 
+{
+  "autoViewerAppLink": {
+    "all": "https://apiautoqa.dropinauto.com/deeplink/viewer"
+  },
+  "autoViewerAppStore": {
+    "ios": "https://waa.ai/auto-qa-ios2",
+    "android": "https://play.google.com/store/apps/developer?id=Dropin+Inc",
+    "ios2": "https://itunes.apple.com/apps/dropininc"
+  },
+  "isOnlyDesktopWeb": false,
+}
---
+++ insurance/qa.js
@@ -0,0 +1,11 @@
+window._appConfig2 = 
+{
+  autoViewerAppLink: {
+		all: 'https://apiautoqa.dropinauto.com/deeplink/viewer',
+	},
+	autoViewerAppStore: {
+		ios: "https://waa.ai/auto-qa-ios2",
+		android: 'https://play.google.com/store/apps/developer?id=Dropin+Inc'
+	},
+  "isOnlyDesktopWeb": false,
+}
