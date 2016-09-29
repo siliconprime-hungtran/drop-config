5161fdfc43e55dbdd679577f76727ff64db4352a
Hung Tran <siliconprime-hungtran@users.noreply.github.com>
2016-09-29 10:38:43 +0700

    Update development.js

--- auto/development.js
+++ auto/development.js
@@ -2,8 +2,8 @@ window._appConfig2 =
 {
  "universalLink": {
     "installLink": {
-      "ios": "https://dashboard.buddybuild.com/download/ios?buildID=57eb834ce4ddb601001aca08&email=hungtran.it3@gmail.com&noNewTab=true",
-      "android": "https://dashboard.buddybuild.com/api/download/download-apk?buildID=57eb92e6f4de1d010098782a&application_variant=devDebug"
+      "ios2": "https://dashboard.buddybuild.com/download/ios?buildID=57eb834ce4ddb601001aca08&email=hungtran.it3@gmail.com&noNewTab=true",
+      "android2": "https://dashboard.buddybuild.com/api/download/download-apk?buildID=57eb92e6f4de1d010098782a&application_variant=devDebug"
     } 
   }
 }
